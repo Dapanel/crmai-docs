5ecda80b0278beadf8ec478772ed0c3d0634cb76
Matoraaa <xzero2975@gmail.com>
2026-08-31 14:46:55 +0700

    feat: add language selection component to navigation bar

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,5 +1,6 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 import Image from "next/image";
+import { LanguageSelect } from "@/components/language-select";
 
 function Logo() {
   return <Image src="/logos.svg" alt="Logo" width={30} height={30} />;
@@ -8,7 +9,8 @@ function Logo() {
 export function getBaseOptions(locale: string): BaseLayoutProps {
   return {
     nav: {
-      title: (
+    children: <LanguageSelect currentLocale={locale as "en" | "id" | "ja" | "ko"} />,
+    title: (
         <>
           <Logo />
           CRMAI Docs
