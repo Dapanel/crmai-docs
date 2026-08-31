abcdfb53fa6c436cd08809a5711c1b2631b64486
Matoraaa <xzero2975@gmail.com>
2026-08-31 11:29:58 +0700

    feat: rebrand project to CRMAI Docs and add WhatsApp support link

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,22 +3,22 @@ import Link from "fumadocs-core/link";
 export default async function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Welcome to Fumadocs</h1>
+      <h1 className="mb-4 text-2xl font-bold">Welcome to CRMAI Docs</h1>
       <p className="text-fd-muted-foreground">
-        This is an example to use Fumadocs with BaseHub.
+        Find thing could be usefull for you.
       </p>
       <div className="flex flex-row items-center justify-center mt-4 gap-4">
         <Link
           href="/docs"
           className="text-fd-foreground hover:text-fd-primary font-medium underline"
         >
-          /docs
+          docs
         </Link>
         <Link
-          href="https://github.com/fuma-nama/fumadocs-basehub"
+          href="https://api.whatsapp.com/send?phone=628113111882"
           className="text-fd-foreground hover:text-fd-primary font-medium underline"
         >
-          /repo
+          Whatsapp
         </Link>
       </div>
     </main>
